3145e352c67c0aa2a815b65e6cc249234b6cf535
Stratulat Stefan <stefan.stratulat@telus.com>
2022-07-27 14:46:24 +0300

    Generate RESTApi template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    val Versions = object {
+        val OPENAPI_GENERERATOR_VERSION = "6.0.1"
+    }
     kotlin("jvm") version "1.7.10"
+    id("org.openapi.generator") version Versions.OPENAPI_GENERERATOR_VERSION
 }
 
 group = "org.telus"
@@ -12,6 +16,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.openapitools:openapi-generator:6.0.1")
+    implementation("org.openapitools:openapi-generator-cli:6.0.1")
+    implementation("org.openapitools:openapi-generator-gradle-plugin:6.0.1")
     testImplementation(kotlin("test"))
 }
 
@@ -20,5 +27,22 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
+}
+
+tasks.openApiGenerate {
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$rootDir/specs/petstore-v3.0.yaml")
+    outputDir.set("$rootDir/src/")
+    apiPackage.set("org.telus.com.api")
+    invokerPackage.set("org.telus.com.invoker")
+    modelPackage.set("org.telus.com.model")
+    configOptions.apply {
+        put("dateLibrary", "java17")
+    }
+    globalProperties.apply {
+        put("modelDocs", "false")
+    }
+    skipValidateSpec.set(true)
+    logToStderr.set(true)
 }
